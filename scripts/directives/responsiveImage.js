7020757771290b7067d35ad347fd29f18b9df18d
Jac21 <jcantu521@gmail.com>
2016-11-14 16:29:18 -0600

    Abstracted out responsive images in photographer view to directive

---
+++ scripts/directives/responsiveImage.js
@@ -0,0 +1,20 @@
+(function() {
+  var app = angular.module('responsive-images', []);
+
+  // UI element directive
+  app.directive('responsiveImage', function($timeout) {
+    return {
+      restrict: 'E',
+      templateUrl: '/templates/directives/responsive-image.html',
+      // https://stackoverflow.com/questions/15207788/calling-a-function-when-ng-repeat-has-finished
+      link: function(scope, element, attr) {
+      	if (scope.$last === true) {
+      		$timeout(function() {
+      			// initialize materialbox
+						$('.materialboxed').materialbox();
+      		})
+      	}
+      }
+    };
+  });
+})();
