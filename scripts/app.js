3cbb6f660df76ee30bb376f9db6b02339db9c371
Jeremy Cantu <jcantu521@gmail.com>
2017-05-09 14:47:48 -0500

    More fixes

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,9 @@
 (function() {
 	'use strict';
 
-	// TODO: https://github.com/angular-ui/ui-router/wiki/Multiple-Named-Views#view-names---relative-vs-absolute-names
-	// TODO: https://scotch.io/tutorials/angular-routing-using-ui-router
 	angular.module('folioApp', ['ui.router', 'responsive-images'])
 		.config(function($stateProvider, $urlRouterProvider, $compileProvider, $locationProvider) {
 
-			// https://code.angularjs.org/1.5.5/docs/guide/production
 		  $compileProvider.debugInfoEnabled(false);
 
 		  $stateProvider
@@ -46,17 +43,6 @@
 		    templateUrl: 'templates/projects/tfstoolset.html',
 		    controller: 'DeveloperController'
 		  });
-
-
-		  /*
-		  	// I swear, there's some perceived slow-down between pages when activating this,
-		  	// opting out until I figure out what that is exactly:
-		  	
-		  $locationProvider.html5Mode({
-		  	enabled: true,
-		  	requireBase: false
-		  });
-		  */
 
 		  $urlRouterProvider.otherwise('/developer');
 		});
