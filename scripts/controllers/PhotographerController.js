07cd9769306d107ced9a11421e0cfada20be4a60
Jeremy Cantu <mail@jeremycantu.com>
2017-07-30 19:11:46 -0500

    Swapped out photos

--- scripts/controllers/PhotographerController.js
+++ scripts/controllers/PhotographerController.js
@@ -9,34 +9,34 @@
 				// image data array
 				$scope.images = [
 				   {
-				      "caption":"Taken at the Hope Outdoor Gallery in Austin, Texas",
-				      "url":"dist/images/photographer/photos/DSC_0063-min.JPG",
-				      "alt":"Graffiti Park"
+				      "caption" : "Taken at the Hope Outdoor Gallery in Austin, Texas",
+				      "url" : "dist/images/photographer/photos/DSC_0063-min.JPG",
+				      "alt" : "Graffiti Park in Austin, Texas"
 				   },
 				   {
-				      "caption":"Taken at Castillo San Cristóbal in San Juan, Puerto Rico",
-				      "url":"dist/images/photographer/photos/DSC_1201.jpg",
-				      "alt":"Puerto Rico"
+				      "caption" : "Taken at Castillo San Cristóbal in San Juan, Puerto Rico",
+				      "url" : "dist/images/photographer/photos/DSC_1201.jpg",
+				      "alt" : "Pier in San Juan, Puerto Rico"
 				   },
 				   {
-				      "caption":"Taken at the Hamilton Pool Preserve",
-				      "url":"dist/images/photographer/photos/DSC_0358-min.JPG",
-				      "alt":"Hamilton Pool Waterfall"
+				      "caption" : "Taken at the Buen Retiro Park in Madrid, Spain",
+				      "url" : "dist/images/photographer/photos/DSC_4544-min.JPG",
+				      "alt" : "Monument to Alfonso XII in Madrid, Spain"
 				   },
 				   {
-				      "caption":"Taken at the Hamilton Pool Preserve",
-				      "url":"dist/images/photographer/photos/DSC_0353-min.JPG",
-				      "alt":"Hamilton Pool Trail"
+				      "caption" : "Taken at the Hamilton Pool Preserve",
+				      "url" : "dist/images/photographer/photos/DSC_0358-min.JPG",
+				      "alt" : "Hamilton Pool Waterfall"
 				   },
 				   {
-				      "caption":"Taken at the Hamilton Pool Preserve",
-				      "url":"dist/images/photographer/photos/DSC_0453-min.JPG",
-				      "alt":"Hamilton Pool Butterfly"
+				      "caption" : "Taken at the Hamilton Pool Preserve",
+				      "url" : "dist/images/photographer/photos/DSC_0453-min.JPG",
+				      "alt" : "Hamilton Pool Butterfly"
 				   },
 				   {
-				      "caption":"Taken at the Mohawk in Austin, Texas (Band: Anamanaguchi)",
-				      "url":"dist/images/photographer/photos/DSC_0631.jpg",
-				      "alt":"Anamanaguchi"
+				      "caption" : "Taken at the Mohawk in Austin, Texas (Band: Anamanaguchi)",
+				      "url" : "dist/images/photographer/photos/DSC_0631.jpg",
+				      "alt" : "Anamanaguchi at the Mohawk"
 				   }
 				];
 				
