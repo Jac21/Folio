658e896efbda16afe4ed3879e5854311a269517b
Jeremy Cantu <jcantu521@gmail.com>
2017-08-10 15:39:01 -0500

    Updates to packages, small fixes

--- scripts/controllers/PhotographerController.js
+++ scripts/controllers/PhotographerController.js
@@ -8,36 +8,36 @@
 
 				// image data array
 				$scope.images = [
-				   {
-				      "caption" : "Taken at the Hope Outdoor Gallery in Austin, Texas",
-				      "url" : "dist/images/photographer/photos/DSC_0063-min.JPG",
-				      "alt" : "Graffiti Park in Austin, Texas"
-				   },
-				   {
-				      "caption" : "Taken at the Hamilton Pool Preserve near Dripping Springs, Texas",
-				      "url" : "dist/images/photographer/photos/DSC_0358-min.JPG",
-				      "alt" : "Hamilton Pool Waterfall near Dripping Springs, Texas"
-				   },
-				   {
-				      "caption" : "Taken at the Buen Retiro Park in Madrid, Spain",
-				      "url" : "dist/images/photographer/photos/DSC_4544-min.JPG",
-				      "alt" : "Monument to Alfonso XII in Madrid, Spain"
-				   },
-				   {
-				      "caption" : "Taken at Castillo San Cristóbal in San Juan, Puerto Rico",
-				      "url" : "dist/images/photographer/photos/DSC_1201.jpg",
-				      "alt" : "Pier in San Juan, Puerto Rico"
-				   },
-				   {
-				      "caption" : "Taken at the Hamilton Pool Preserve near Dripping Springs, Texas",
-				      "url" : "dist/images/photographer/photos/DSC_0453-min.JPG",
-				      "alt" : "Hamilton Pool Butterfly near Dripping Springs, Texas"
-				   },
-				   {
-				      "caption" : "Taken at the Mohawk in Austin, Texas (Band: Anamanaguchi)",
-				      "url" : "dist/images/photographer/photos/DSC_0631.jpg",
-				      "alt" : "Anamanaguchi at the Mohawk in Austin, Texas"
-				   }
+				{
+					"caption" : "Taken at the Hope Outdoor Gallery in Austin, Texas",
+					"url" : "dist/images/photographer/photos/DSC_0063-min.JPG",
+					"alt" : "Graffiti Park in Austin, Texas"
+				},
+				{
+					"caption" : "Taken at the Hamilton Pool Preserve near Dripping Springs, Texas",
+					"url" : "dist/images/photographer/photos/DSC_0358-min.JPG",
+					"alt" : "Hamilton Pool Waterfall near Dripping Springs, Texas"
+				},
+				{
+					"caption" : "Taken at the Buen Retiro Park in Madrid, Spain",
+					"url" : "dist/images/photographer/photos/DSC_4544-min.JPG",
+					"alt" : "Monument to Alfonso XII in Madrid, Spain"
+				},
+				{
+					"caption" : "Taken at Castillo San Cristóbal in San Juan, Puerto Rico",
+					"url" : "dist/images/photographer/photos/DSC_1201.jpg",
+					"alt" : "Pier in San Juan, Puerto Rico"
+				},
+				{
+					"caption" : "Taken at the Hamilton Pool Preserve near Dripping Springs, Texas",
+					"url" : "dist/images/photographer/photos/DSC_0453-min.JPG",
+					"alt" : "Hamilton Pool Butterfly near Dripping Springs, Texas"
+				},
+				{
+					"caption" : "Taken at the Mohawk in Austin, Texas (Band: Anamanaguchi)",
+					"url" : "dist/images/photographer/photos/DSC_0631.jpg",
+					"alt" : "Anamanaguchi at the Mohawk in Austin, Texas"
+				}
 				];
 				
 				// utility function for linking between views
@@ -45,8 +45,7 @@
 				  $location.path( path );
 				};
 
-				// utility function to set viewport to the top of the page 
-				// on element click
+				// utility function to set viewport to the top of the page on element click
 				$scope.backToTop = function() {
 					$anchorScroll();
 				};
