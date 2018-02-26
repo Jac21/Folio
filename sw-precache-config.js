a555356c3d629503842140add35c2e75955db6d3
Jeremy Cantu <mail@jeremycantu.com>
2018-02-25 22:15:51 -0600

    Precache updates

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,17 +1,13 @@
 module.exports = {
   staticFileGlobs: [
-    '**.html',
-    '*.js',
-    'templates/**',
-    'scripts/lib/**.js',
-    'scripts/directives/*.js',
-    'scripts/*.js',
-    'fonts/**',
-    'dist/styles/style.css',
-    'dist/styles/materialize.min.css',
-    'dist/stylesUncss/material-design-iconic-font.css',
-    'dist/images/**',
-    'dist/scripts/*'
+    "**.html",
+    "templates/**",
+    "dist/fonts/**",
+    "dist/styles/style.css",
+    "dist/styles/materialize.min.css",
+    "dist/stylesUncss/material-design-iconic-font.css",
+    "dist/images/**",
+    "dist/scripts/**"
   ],
-  navigateFallback: '/index.html'
+  navigateFallback: "/index.html"
 };
