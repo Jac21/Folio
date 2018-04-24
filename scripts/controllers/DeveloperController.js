afdb30ea7eb616fda3efc33daa96a5d16e29046c
Jeremy Cantu <jcantu521@gmail.com>
2018-04-24 16:58:35 -0500

    Fix blog page images not being 'materialbox'-ed

--- scripts/controllers/DeveloperController.js
+++ scripts/controllers/DeveloperController.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   "use strict";
 
   angular // eslint-disable-line no-undef
@@ -7,12 +7,12 @@
       "$scope",
       "$location",
       "$anchorScroll",
-      function ($scope, $location, $anchorScroll) {
+      function($scope, $location, $anchorScroll) {
         // set page class
         $scope.pageClass = "developer";
 
         // utility function for linking between views
-        $scope.go = function (path) {
+        $scope.go = function(path) {
           $location.path(path);
         };
 
@@ -27,20 +27,21 @@
         };
 
         $scope.fabButtonLinks = {
-          email: "mailto:mail@jeremycantu.com?subject=Jeremy Cantu's Portfolio Inquiry&body=Let's talk!",
+          email:
+            "mailto:mail@jeremycantu.com?subject=Jeremy Cantu's Portfolio Inquiry&body=Let's talk!",
           file: "https://jac21.github.io",
           github: "https://www.github.com/Jac21",
           linkedin: "https://www.linkedin.com/in/jeremycantu"
         };
 
         // utility function to add active css class to target element
-        $scope.activate = function (event) {
+        $scope.activate = function(event) {
           $(event.target).addClass("active"); // eslint-disable-line no-undef
         };
 
         // utility function to set viewport to the top of the page
         // on element click
-        $scope.backToTop = function () {
+        $scope.backToTop = function() {
           $anchorScroll();
         };
 
@@ -52,13 +53,21 @@
           hoverEnabled: false
         });
 
-        // initialize blog container wrapper style
-        var secondaryThemeHexCode = '#ffab40';
+        // initialize materialbox
+        var elem = document.querySelectorAll(".materialboxed");
+        var instance = M.Materialbox.init(elem, {
+          inDuration: 275,
+          outDuration: 200
+        });
 
-        if (document.querySelector('div.container.blog-container')) {
-          document.body.parentNode.style.boxShadow = "0 0 0 5px " + secondaryThemeHexCode + " inset";
+        // initialize blog container wrapper style
+        var secondaryThemeHexCode = "#ffab40";
+
+        if (document.querySelector("div.container.blog-container")) {
+          document.body.parentNode.style.boxShadow =
+            "0 0 0 5px " + secondaryThemeHexCode + " inset";
         } else {
-          document.body.parentNode.style.boxShadow = '';
+          document.body.parentNode.style.boxShadow = "";
         }
       }
     ]);
