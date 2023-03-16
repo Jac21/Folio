b79479c5364434db3a45d0d16545ebd8bb753c24
Jeremy Cantu <jcantu521@gmail.com>
2023-03-16 16:25:44 -0500

    Photography-associated updates

--- scripts/controllers/PhotographerController.js
+++ scripts/controllers/PhotographerController.js
@@ -14,44 +14,46 @@
         // page specific objects
         $scope.photographerHeadshot = {
           url: "dist/images/photographer/headshot-photo.jpg",
-          alt: "photographer headshot"
+          alt: "photographer headshot",
         };
 
         $scope.photographerHeaderLink = {
-          url: "https://www.jcantuphotography.com/"
+          url: "https://www.jcantuphotography.com/",
         };
 
         // image data array
-        $scope.images = [{
+        $scope.images = [
+          {
+            caption:
+              "Taken at the Lady Bird Johnson Wildflower Center in Austin, Texas",
+            url: "dist/images/photographer/photos/DSC_63960011.JPG",
+            alt: "Lady Bird Johnson Wildflower Center in Austin, Texas",
+          },
+          {
+            caption: "Taken at the Charco El Hippie in Puerto Rico",
+            url: "dist/images/photographer/photos/DSC_204160025.JPG",
+            alt: "Charco El Hippie in Puerto Rico",
+          },
+          {
             caption: "Taken at Avila in Spain",
             url: "dist/images/photographer/photos/DSC_4705.JPG",
-            alt: "Avila in Spain"
+            alt: "Avila in Spain",
           },
           {
             caption: "Taken in Martindale, Texas",
             url: "dist/images/photographer/photos/DSC_4110.JPG",
-            alt: "Martindale, Texas"
+            alt: "Martindale, Texas",
           },
           {
             caption: "Taken at the Buen Retiro Park in Madrid, Spain",
             url: "dist/images/photographer/photos/DSC_4544.JPG",
-            alt: "Monument to Alfonso XII in Madrid, Spain"
+            alt: "Monument to Alfonso XII in Madrid, Spain",
           },
           {
             caption: "Taken at Castillo San Cristóbal in San Juan, Puerto Rico",
             url: "dist/images/photographer/photos/DSC_1201.jpg",
-            alt: "Pier in San Juan, Puerto Rico"
+            alt: "Pier in San Juan, Puerto Rico",
           },
-          {
-            caption: "Taken at the Hamilton Pool Preserve near Dripping Springs, Texas",
-            url: "dist/images/photographer/photos/DSC_0358.JPG",
-            alt: "Hamilton Pool Waterfall near Dripping Springs, Texas"
-          },
-          {
-            caption: "Taken at the Mohawk in Austin, Texas (Band: Anamanaguchi)",
-            url: "dist/images/photographer/photos/DSC_0631.jpg",
-            alt: "Anamanaguchi at the Mohawk in Austin, Texas"
-          }
         ];
 
         // utility function for linking between views
@@ -63,6 +65,6 @@
         $scope.backToTop = function () {
           $anchorScroll();
         };
-      }
+      },
     ]);
 })();
